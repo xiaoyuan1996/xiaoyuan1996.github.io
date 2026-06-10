a5b4f1fd1b24a26eae02e34c0354f121ce7d979e
Govern <36145159+xiaoyuan1996@users.noreply.github.com>
2026-06-10 16:56:41 +0800

    Delete files directory

--- files/walkvlm/main.js
+++
@@ -1,9 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-$(document).ready(function() {
-    $('.video-container').on('wheel', function(e) {
-        e.preventDefault();
-        this.scrollLeft += e.originalEvent.deltaY;
-    });
-});
-</script>
